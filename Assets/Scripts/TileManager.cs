e25d4410b5edd00878c2b558ecd91341010e26fe
lennartvandorp <57751002+lennartvandorp@users.noreply.github.com>
2020-10-14 15:02:27 +0200

    Hex tile aanpassingen

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileManager: MonoBehaviour
+{
+    public Transform middleTilePoint;
+    public TileBase middleTileSprite;
+
+    public Tilemap tilemap;
+
+    Vector3Int middleTile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        middleTile = tilemap.WorldToCell(middleTilePoint.position);
+        tilemap.SetTile(middleTile, middleTileSprite);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
