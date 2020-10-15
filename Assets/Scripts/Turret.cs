2c88305a2c0badf98715c583670e490dec1ee5c9
bossie911 <62554007+bossie911@users.noreply.github.com>
2020-10-15 20:00:13 +0200

    turrets kunnen nu hun target zoeken, heb van de enemy een prefab gemaakt en deze prefab gemarkeerd met een tag

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
+
+    public Transform target;
+
+
+    public float turretRange = 5f;
+
+
+    public string targetTag = "enemy";
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,46 @@ public class Turret : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        FindTarget();           
     }
+
+    //Functie dat de dichtbijzijnste enemy zoekt en markt als target
+    void FindTarget()
+    {    
+        //array voor enemy gameobjects
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(targetTag);
+
+        float closestDistance = Mathf.Infinity;
+        GameObject closestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < closestDistance)
+            {
+                closestDistance = distanceToEnemy;
+                closestEnemy = enemy;
+            }
+        }
+
+        //Onderstaande code zorgt ervoor dat als de dichtbijzijnste enemy in range is van de turret deze enemy als target wordt gemarkeerd
+        if (closestEnemy != null && closestDistance <= turretRange)
+        {
+            target = closestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+
+
+    //Range drawen
+    void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, turretRange);
+    }
+
 }
