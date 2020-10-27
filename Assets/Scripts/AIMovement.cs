bb84c33cef8dfdc2c1310645eaba91c60a495b87
lennartvandorp <57751002+lennartvandorp@users.noreply.github.com>
2020-10-27 14:59:17 +0100

    Pathfinding basics

---
+++ Assets/Scripts/AIMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform middle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        agent.SetDestination(middle.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this is not efficient we should make it so that it only updates when the target changes
+    }
+
+    
+}
