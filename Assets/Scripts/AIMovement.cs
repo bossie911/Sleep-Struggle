b0c3607b9c8dd35c479a436da57d4d1ac5ecd8ff
lennartvandorp <57751002+lennartvandorp@users.noreply.github.com>
2020-10-27 15:44:09 +0100

    Enemy spawning

--- Assets/Scripts/AIMovement.cs
+++ Assets/Scripts/AIMovement.cs
@@ -6,14 +6,12 @@ using UnityEngine.AI;
 public class AIMovement : MonoBehaviour
 {
     NavMeshAgent agent;
-    public Transform middle;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
-        agent.SetDestination(middle.position);
     }
 
     // Update is called once per frame
@@ -22,5 +20,6 @@ public class AIMovement : MonoBehaviour
         //this is not efficient we should make it so that it only updates when the target changes
     }
 
+    
     
 }
---
+++ Assets/Scripts/Game Management/EnemyManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject regularEnemy;
+    public float timeBetweenEnemies;
+    float enemiesTimer;
+    public Transform[] spawnPoint;
+    public Transform parent;
+    public Transform middle;
+    int whereToSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        enemiesTimer += Time.deltaTime;
+        if (enemiesTimer >= timeBetweenEnemies)
+        {
+            whereToSpawn = Random.Range(0,2);
+            enemiesTimer = 0;
+            GameObject newGuy = Instantiate(regularEnemy, spawnPoint[whereToSpawn].position, Quaternion.identity);//Creates a new enemy
+            newGuy.transform.SetParent(parent);//orders the enemy to avoid cluttering
+            newGuy.GetComponent<NavMeshAgent>().SetDestination(middle.position);//sets the destination of the enemy
+        }
+    }
+
+
+}
