75e21fa1fec4531c380073d0ed7ac84a8df6eb62
lennartvandorp <57751002+lennartvandorp@users.noreply.github.com>
2020-11-11 09:34:31 +0100

    Generation deels gemaakt. Druk op F tijdens runtime om een visuele map te generaten.

---
+++ Assets/Scripts/Game Management/TestNoise.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestNoise : MonoBehaviour
+{
+
+    Renderer renderer;
+
+    int width, height;
+    // Start is called before the first frame update
+    void Start()
+    {
+        width = height = 256;
+        renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = GetTexture();
+    }
+
+    Texture2D GetTexture()
+    {
+        Texture2D texture = new Texture2D(width, height);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; x < height; y++)
+            {
+                texture.SetPixel(x, y, GetColor(x, y));
+            }
+
+        }
+        texture.Apply();
+        return texture;
+    }
+
+    Color GetColor(int x, int y)
+    {
+        float xSample = (float)x / width;
+        float ySample = (float)y / height;
+
+        float shade = Mathf.PerlinNoise(xSample, ySample);
+        return new Color(shade, shade, shade);
+
+    }
+
+}
---
+++ Assets/Scripts/Game Management/TileManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class TileManager : MonoBehaviour
+{
+    public Transform middleTilePoint;
+    public TileBase middleTileSprite;
+    public TileBase[] differentTiles;
+
+    public Tilemap tilemap;
+
+    Vector3Int middleTile;
+
+    int[,] tileTypes;
+
+    public int width, height;
+
+    public float scale;
+    Random random;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        middleTile = tilemap.WorldToCell(middleTilePoint.position);
+
+        tilemap.SetTile(middleTile, middleTileSprite);
+
+        tileTypes = new int[width, height];
+
+        for (int x = middleTile.x - width / 2; x < middleTile.x + width / 2; x++)
+        {
+            for (int y = middleTile.y - height / 2; y < middleTile.y + height / 2; y++)
+            {
+                tilemap.SetTile(new Vector3Int(x,y,0), middleTileSprite);
+            }
+        }
+        random = new Random();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            SetTiles(); // if f is pressed, the generation will occur
+        }
+
+    }
+
+    public int[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, float xOffset, float yOffset)
+    {
+        int[,] noiseMap = new int[mapWidth, mapHeight];
+
+        for (int x = 0; x < mapWidth; x++)
+        {
+            for (int y = 0; y < mapHeight; y++)
+            {
+                float sampleX = (float)x / mapWidth * scale;
+                float sampleY = (float)y / mapHeight * scale;
+                float noise = Mathf.PerlinNoise(sampleX, sampleY) * differentTiles.Length;
+                noiseMap[x, y] = (int)noise;
+            }
+        }
+        return noiseMap;
+    }
+
+    void SetTiles()
+    {
+        tileTypes = GenerateNoiseMap(width, height, scale, 1, 1);
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                tilemap.SetTile(new Vector3Int(x - width / 2, y - height / 2, 0), differentTiles[tileTypes[x, y]]);
+            }
+
+        }
+    }
+}
+
