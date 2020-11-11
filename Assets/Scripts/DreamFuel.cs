c6b7688c52818017ca2e4fdd630752ade092f7da
wessels8 <wessels8@ad.hva.nl>
2020-11-11 14:35:45 +0100

    Added a UI in siem's scene that makes the player able to see his DreamFuel amount. also added a DreamFuel (resource) script

---
+++ Assets/Scripts/DreamFuel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DreamFuel : MonoBehaviour
+{
+    public float minimumResourceValue = 0f;
+    public float currentResourceValue = 50f;
+
+    public float baseGeneration = 1f;
+    public float generationDelay = 1f;
+    protected float generationTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ResourceGeneration();
+    }
+
+    //ResourceGeneration manages the amount of DreamFuel that is added per x amount of time.
+    //Have to test what the right starting amount is and what the right base generation amount is.
+    public void ResourceGeneration()
+    {
+        generationTimer += Time.deltaTime;
+
+        if(generationTimer >= generationDelay)
+        {
+            currentResourceValue += baseGeneration;
+            Debug.Log(currentResourceValue);
+            generationTimer = 0f;
+        }
+    }
+
+    public void TurretPlaced()
+    {
+        //This script gets called when the player places a turret of said type. 
+        //The dreamfuel cost has to be subtracted from the currentResourceValue
+        //currentResourceValue has to then be updated
+    }
+
+    public void DreamFactoryPlaced()
+    {
+        //This script is different than the TurretPlaced script in the fact that it 
+        //also adds an x amount to baseGeneration within the ResourceGeneration Function
+        //The dreamfuel cost has to be subtracted from the currentResourceValue
+        //currentResourceValue has to then be updated
+    }
+}
