308a66ffba35ab390e032d6f07068755ca103ecc
bossie911 <62554007+bossie911@users.noreply.github.com>
2020-11-12 10:15:52 +0100

    HP bar base

---
+++ Assets/Scripts/HPBarBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HPBarBase : MonoBehaviour
+{
+    private Slider slider;
+    public Image fillimage;
+
+    public static float baseHP = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        slider.value = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = baseHP;
+    }
+}
