1da0864b211dd58c3625f07c559a7bb401eecff9
nordi <nordin.elhadaoui@gmail.com>
2020-11-16 22:03:58 +0100

    Revert "Healthbar beginsel"
    This reverts commit 7466df8519c3e8d314df520d1b7502cd75860b55.

--- Assets/Scripts/HPBarBase.cs
+++ Assets/Scripts/HPBarBase.cs
@@ -11,21 +11,16 @@ public class HPBarBase : MonoBehaviour
 
     public static float baseHP = 100;
 
-    BaseStats Castle;
-
     // Start is called before the first frame update
     void Start()
     {
         slider = GetComponent<Slider>();
         slider.value = 100;
-        Castle = FindObjectOfType<BaseStats>();
     }
 
     // Update is called once per frame
     void Update()
     {
         slider.value = baseHP;
-
-        baseHP = Castle.Health;
     }
 }
