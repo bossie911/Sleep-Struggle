7466df8519c3e8d314df520d1b7502cd75860b55
nordi <nordin.elhadaoui@gmail.com>
2020-11-16 21:57:42 +0100

    Healthbar beginsel

---
+++ Assets/BaseStats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseStats : MonoBehaviour
+{
+    public float Health = 100f;
+
+    public string targetTag = "enemy";
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+
+        Debug.Log(Health);
+
+        if (Health <= 1)
+        {
+            
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("enemy"))
+            {
+                Health -= 1;
+            }
+        }
+}
