746905eb904efa1ca7315684afe3f68a349a9972
lennartvandorp <57751002+lennartvandorp@users.noreply.github.com>
2020-12-04 14:59:44 +0100

    Audiomanager en Temp muziek

---
+++ Assets/Scripts/Game Management/AudioManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioObject[] objects;
+    public GameObject boss;
+
+    public bool LoopMusic;
+
+    bool hasStarted;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach (AudioObject a in objects)
+        {
+            a.setSource(gameObject.AddComponent<AudioSource>());
+            a.getSource().pitch = a.pitch;
+            a.getSource().volume = a.volume;
+            a.getSource().clip = a.audioClip;
+        }
+        Play("music");
+    }
+
+
+    void Update()
+    {
+        if (LoopMusic)
+        {
+            foreach (AudioObject a in objects)
+            {
+                if (a.name == "music")
+                {
+                    AudioSource source = a.getSource();
+                    if (!source.isPlaying && hasStarted)
+                    {
+                        Play("music");
+                    }
+                }
+            }
+        }
+    }
+
+    public void Play(string name)
+    {
+        AudioObject a = Array.Find(objects, AudioObject => AudioObject.name == name);
+        a.getSource().Play();
+    }
+
+
+
+}
