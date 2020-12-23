6c7185bb7b304dbea6bbc9354f4956447d1ada9f
Int3gerz <jonah.h2012@gmail.com>
2020-12-23 16:50:21 +0100

    Revert "Revert "Small tweaks to the base class""
    This reverts commit 4ebe3fa53ddf6ce97b837b21f6ed17bd15f4b79b.

--- Assets/PlaceTurret.cs
+++ Assets/PlaceTurret.cs
@@ -13,10 +13,11 @@ using UnityEngine.Analytics;
 public class PlaceTurret : MonoBehaviour
 {
     public Tilemap tilemap;
-    public GameObject turret, factory, DreamFuel, mine;
+    public GameObject turretPrefab, factoryPrefab, DreamFuel, mine;
     public Tilemap fogOfWar;
-
+   
     TileManager manager;
+    
     static DreamFactorySelector selector;
     private int fow_layer; 
 
@@ -28,6 +29,7 @@ public class PlaceTurret : MonoBehaviour
     static Vector3 turretOffset, factoryOffset, mineOffset;
 
     public float resourceCost;
+    public float factoryCost;
 
     public float factoryAddedGeneration = 0.5f;
     float factoryBaseCost = 30;
@@ -85,16 +87,15 @@ public class PlaceTurret : MonoBehaviour
                 switch (selector.SelectedPlaceable)
                 {
                     case DreamFactorySelector.Placeables.Turret:
-                        Place(point, currentTile, turret, currentOffset);
+                        Place(point, currentTile, turretPrefab, currentOffset);
                         break;
 
                     case DreamFactorySelector.Placeables.Factory:
-                        Place(point, currentTile, factory, currentOffset);
+                        Place(point, currentTile, factoryPrefab, currentOffset);
                         break;
 
                     case DreamFactorySelector.Placeables.Mine:
                         Place(point, currentTile, mine, currentOffset);
-
                         break; 
                 }
             }
@@ -121,30 +122,20 @@ public class PlaceTurret : MonoBehaviour
     {
         Vector3 worldPosition = tilemap.CellToWorld(tilemap.WorldToCell(point));
 
-        //Get all turrets currently on the field
-        //count = Resources.FindObjectsOfTypeAll<GameObject>().Count(obj => obj.name == "Turret");
-
-        resourceCost = 0f;
-        if (Equals(towerToPlace, turret))
+        if (Equals(towerToPlace, turretPrefab))
         {
-            resourceCost = 50f;
-            GameObject newTurret = Instantiate(turret, worldPosition + offset, Quaternion.identity);
+            GameObject newTurret = Instantiate(turretPrefab, worldPosition + offset, Quaternion.identity);
 
             //Create all turrets as a child of this gameobj, so the hierarchy doesn't get cluttered
             newTurret.transform.SetParent(this.transform);
+            Turret turret = newTurret.GetComponent<Turret>(); 
 
             //Set reference FoW of the newly created turret
-            newTurret.GetComponent<Turret>().FogOfWar = fogOfWar;
-            DreamFuel.GetComponent<DreamFuel>().currentResourceValue -= resourceCost;
-
-            /*AnalyticsEvent.Custom("TurretsBuild", new Dictionary<string, object>
-            {
-                {$"(Turret number = {count}", count},
-                {"Time_elapsed", Time.timeSinceLevelLoad}
-            }); */
-
+            turret.FogOfWar = fogOfWar;
+            turret.PayResourceCost(DreamFuel);
         }
-        else if(Equals(towerToPlace, factory))
+
+        else if(Equals(towerToPlace, factoryPrefab))
         {
             //Code voor factories die steeds meer kosten
             factoryExtraCost += factoryExtraCostPerFactory;
@@ -153,7 +144,7 @@ public class PlaceTurret : MonoBehaviour
             resourceCost = factoryTotalCost;
             Debug.Log(resourceCost);
 
-            GameObject newFactory = Instantiate(factory, worldPosition + offset, Quaternion.identity);
+            GameObject newFactory = Instantiate(factoryPrefab, worldPosition + offset, Quaternion.identity);
 
             newFactory.transform.SetParent(this.transform);
 
@@ -161,13 +152,8 @@ public class PlaceTurret : MonoBehaviour
 
             DreamFuel.GetComponent<DreamFuel>().currentResourceValue -= resourceCost;
             DreamFuel.GetComponent<DreamFuel>().baseGeneration += 1f;//factoryAddedGeneration;
-
-            //TODO
-            //Analytics.CustomEvent("FactoriesBuilt", new Dictionary<string, object>
-            //{
-            //    {$"Turret number = {this.transform.childCount}", this.transform.childCount}
-            //});
         }
+
         else if(Equals(towerToPlace, mine)) {
             resourceCost = mineCost;
             GameObject newMine = Instantiate(mine, worldPosition + offset, Quaternion.identity);
