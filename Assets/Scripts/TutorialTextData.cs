efc57eaa1e6bcc6d63e527c856a016bb6c1ffb81
lennartvandorp <57751002+lennartvandorp@users.noreply.github.com>
2020-12-28 15:27:37 +0100

    Tutorial text systeem neergezet, vergelijkbaar met het wavesysteem.

---
+++ Assets/Scripts/TutorialTextData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class TutorialTextData
+{
+    public float timeVisible;
+    public GameObject tutorial;
+    Text text;
+    TutorialTextData()
+    {
+    }
+    public void Setup()
+    {
+        text = tutorial.GetComponent<Text>();
+    }
+
+    public void EnableText(bool enable)
+    {
+        if (enable)
+        {
+            text.enabled = true;
+            return;
+        }
+        else
+        {
+            text.enabled = false;
+            return;
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/TutorialTextManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTextManager : MonoBehaviour
+{
+    public TutorialTextData[] tutorials;
+    int currentTutorial;
+    float tutorialTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < tutorials.Length; i++)
+        {
+            tutorials[i].Setup();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tutorials.Length > currentTutorial)
+        {
+            tutorialTimer += Time.deltaTime;
+            if (tutorialTimer > tutorials[currentTutorial].timeVisible)
+            {
+                tutorials[currentTutorial].EnableText(false);
+                tutorialTimer = 0;
+                currentTutorial++;
+            }
+        }
+    }
+}
