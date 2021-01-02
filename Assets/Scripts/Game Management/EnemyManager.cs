b359c5e122164fab383eb7c7df9c79362f4ceb8f
lennartvandorp <57751002+lennartvandorp@users.noreply.github.com>
2021-01-02 15:03:28 +0100

    Waves endless gemaakt

--- Assets/Scripts/Game Management/EnemyManager.cs
+++ Assets/Scripts/Game Management/EnemyManager.cs
@@ -24,6 +24,7 @@ public class EnemyManager : MonoBehaviour
 
     float totalWaveTime, gameEndTimer;
     public Text victoryText;
+    public bool endless;
 
 
     // Start is called before the first frame update
@@ -35,7 +36,8 @@ public class EnemyManager : MonoBehaviour
         });
         Debug.Log("start:" + result);
 
-        for (int i = 0; i < waves.Length; i++) {
+        for (int i = 0; i < waves.Length; i++)
+        {
             totalWaveTime += waves[i].waveDurationSeconds;
             totalWaveTime += waves[i].cooldownAfterWave;
         }
@@ -52,21 +54,27 @@ public class EnemyManager : MonoBehaviour
             cooldownActive = true;
         }
 
-        if (waveTimer >= waves[currentWave].cooldownAfterWave + waves[currentWave].waveDurationSeconds && currentWave < waves.Length -1)
-        {
-            if (currentWave > waves.Length - 1) {
-                AnalyticsResult result = Analytics.CustomEvent("CompleteLevel", new Dictionary<string, object> 
+            if (waveTimer >= waves[currentWave].cooldownAfterWave + waves[currentWave].waveDurationSeconds && currentWave < waves.Length - 1)
+            {
+                if (currentWave > waves.Length - 1)
+                {
+                    AnalyticsResult result = Analytics.CustomEvent("CompleteLevel", new Dictionary<string, object>
                 {{"CompleteLevel", 1}
                 });
 
-                Debug.Log("leveldone : " + result);
+                    Debug.Log("leveldone : " + result);
+                }
+                currentWave++;
+                if(endless && currentWave > waves.Length -1){
+                    currentWave = waves.Length -1;
+                }
+                startWave();
             }
-            currentWave++;
-            startWave();
-        }
+        
 
         gameEndTimer += Time.deltaTime;
-        if (gameEndTimer > totalWaveTime) {
+        if (gameEndTimer > totalWaveTime)
+        {
             //victoryText.enabled = true;
         }
 
@@ -78,13 +86,14 @@ public class EnemyManager : MonoBehaviour
 
             nextRegularEnemyTime += timeBetweenRegularEnemies;
         }
-        
+
         if (!cooldownActive && waveTimer >= nextMosquitoTime)
         {
             spawnEnemy(mosquito);
 
             nextMosquitoTime += timeBetweenMosquito;
         }
+
     }
 
     /// starts a new wave and calculates the values needed
@@ -92,9 +101,11 @@ public class EnemyManager : MonoBehaviour
     {
         waveTimer = 0;
         timeBetweenRegularEnemies = waves[currentWave].waveDurationSeconds / waves[currentWave].amountOfNormalEnemies;
+        //calculates how often enemies are spawned
         nextRegularEnemyTime = timeBetweenRegularEnemies;
-        
+
         timeBetweenMosquito = waves[currentWave].waveDurationSeconds / waves[currentWave].amountOfMosquitos;
+        //calculates how often mosquitos are spawned
         nextMosquitoTime = timeBetweenMosquito;
 
         cooldownActive = false;
