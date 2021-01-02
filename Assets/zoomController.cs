3cca9ce5dcb88bbf7bc28c5cd3bed28967abe455
nordi <nordin.elhadaoui@gmail.com>
2021-01-02 21:21:37 +0100

    Je kan nu met scroller bewegen

--- Assets/zoomController.cs
+++ Assets/zoomController.cs
@@ -4,25 +4,53 @@ using UnityEngine;
 
 public class zoomController : MonoBehaviour
 {
+    [SerializeField]
     private Camera cam;
-    private float targetZoom; 
+
+    [SerializeField]
+    private float zoomLerpSpeed;
+
+    private Vector3 dragOrigin;
+
+
+    private float targetZoom;
     private float zoomFactor = 3f;
-    [SerializeField] private float zoomLerpSpeed; 
 
     void Start()
     {
         cam = Camera.main;
-        targetZoom = cam.orthographicSize; 
+        targetZoom = cam.orthographicSize;
     }
 
-    void Update()
+    private void Update()
     {
         float scrollData;
         scrollData = Input.GetAxis("Mouse ScrollWheel");
 
         targetZoom -= scrollData * zoomFactor;
-        targetZoom = Mathf.Clamp(targetZoom, 4f, 9f); 
-        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomLerpSpeed); 
+        targetZoom = Mathf.Clamp(targetZoom, 4f, 9f);
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomLerpSpeed);
 
+        PanCamera();
+    }
+
+    private void PanCamera()
+    {
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        }
+
+        if (Input.GetMouseButton(2))
+        {
+            Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
+
+            print(" origin " + dragOrigin + " newPosition " + cam.ScreenToWorldPoint(Input.mousePosition) + " =difference " + difference);
+
+            cam.transform.position += difference;
+
+        }
     }
 }
