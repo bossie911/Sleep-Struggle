86727a6b5dbe0c47daf666bf6b31cb0ec49be5f8
lennartvandorp <57751002+lennartvandorp@users.noreply.github.com>
2021-01-04 16:03:01 +0100

    Level 3 basics

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Portal : MonoBehaviour
 {
     public float portalHealth = 500;
+    public AudioManager audioManager;
 
+    public GameObject deathParticles;
+    float lastHealth;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -18,10 +21,17 @@ public class Portal : MonoBehaviour
     {
         if (portalHealth <= 0)
         {
+            Instantiate(deathParticles, transform.position, Quaternion.identity);//starts the blood splatter animation
+            audioManager.Play("portalBreak");
             Destroy(this.gameObject);
         }
 
 
         //Debug.Log(portalHealth);
+        if (lastHealth > portalHealth)
+        {
+            audioManager.Play("portalDamage");
+        }
+        lastHealth = portalHealth;
     }
 }
